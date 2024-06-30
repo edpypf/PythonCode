dd9dce57b0c4260468e5d6bb0a6d7bb8bb36494f
edpypf <edpypf@gmail.com>
2024-06-30 13:54:57 -0400

    Create FactoryConcept.py

---
+++ DesignPattern/FactoryConcept.py
@@ -0,0 +1,27 @@
+# Factory is to move too many factory method out to be another group of methods, which is another class
+from cmath import cos, sin
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'x: {self.x}, y: {self.y}'        
+
+class PointFactory:
+    @staticmethod
+    def new_cartesian_point(x, y):
+        p = Point()
+        p.x = x
+        p.y = y
+        return p
+    
+    @staticmethod
+    def new_polar_point(rho, theta):
+        return Point(rho * cos(theta), rho * sin(theta))
+
+if __name__ == '__main__':
+    p = Point(2, 3)
+    p2 = PointFactory.new_polar_point(1, 2)
+    print(p, p2)
